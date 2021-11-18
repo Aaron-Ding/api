98563f1f6b480272e6a37c41db7a9eb6b7547546
aaron-ding <aaronding1990@gmail.com>
2021-11-18 18:54:51 -0500

    fix again

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,16 @@ Route::group(['namespace' => 'api'], function () {
     Route::post('/login', '\App\Http\Controllers\AuthController@loginUser');
     Route::middleware('auth:api')->group(function () {
         Route::group(['namespace' => 'leaderboard'], function () {
-            Route::get('/leaderboard/users', '\App\Http\Controllers\LeaderBoardController@index')->name('all.user');
-            Route::get('/leaderboard/user/{id}', '\App\Http\Controllers\LeaderBoardController@get')->name('all.user');
+            Route::get('/leaderboard/guests', '\App\Http\Controllers\LeaderBoardController@index')->name('all.user');
+            Route::get('/leaderboard/guest/{id}', '\App\Http\Controllers\LeaderBoardController@get')->name('all.user');
 
-            Route::delete('/leaderboard/user/{id}', '\App\Http\Controllers\LeaderBoardController@delete')->name(
+            Route::delete('/leaderboard/guest/{id}', '\App\Http\Controllers\LeaderBoardController@delete')->name(
                 'delete.user'
             );
-            Route::post('/leaderboard/user/{id}', '\App\Http\Controllers\LeaderBoardController@update')->name(
+            Route::post('/leaderboard/guest/{id}', '\App\Http\Controllers\LeaderBoardController@update')->name(
                 'update.user'
             );
-            Route::post('/leaderboard/user/', '\App\Http\Controllers\LeaderBoardController@create')->name(
+            Route::post('/leaderboard/guest/', '\App\Http\Controllers\LeaderBoardController@create')->name(
                 'create.user'
             );
         });
